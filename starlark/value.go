569f749f7817e26028bb5925cb2927dd74447915
Ed Jones <ed.jones@canonical.com>
2022-08-19 14:46:33 +0100

    Documentation comment fix

--- starlark/value.go
+++ starlark/value.go
@@ -750,8 +750,8 @@ func NewBuiltin(name string, fn func(thread *Thread, fn *Builtin, args Tuple, kw
 
 // NewBuiltinWithSafety is a convenience function which, like NewBuiltin,
 // returns a new `builtin_function_or_method` with the specified name and
-// implementations, which compares unequal with all other values. The builtin
-// returned also has the specified set of safety flags.
+// implementation, which compares unequal with all other values.
+// NewBuiltinWithSafety also declares the safety of implementation function fn.
 //
 // This function is equivalent to calling NewBuiltin and DeclareSafety on its
 // result.
