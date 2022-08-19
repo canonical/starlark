d91fbe07fd749f3d83a0a319160654f644f042cd
Ed Jones <ed.jones@canonical.com>
2022-08-19 14:37:56 +0100

    Comment grammar fixes

--- starlark/safety.go
+++ starlark/safety.go
@@ -45,7 +45,7 @@ func (f SafetyFlags) Format(state fmt.State, verb rune) {
 // A pointer to a function of any type.
 type function uintptr
 
-// Central map of functions to their safeties.
+// Central map of functions to their declared safety flags.
 //
 // Note that as closures share the same underlying function, all instances of a
 // given closure will implicitly share the same safety flags
@@ -59,7 +59,7 @@ func init() {
 	}
 }
 
-// Get the names of the flags set in a given SafetyFlags instance
+// Names returns a list of human-readable names of set flags
 func (flags SafetyFlags) Names() (names []string) {
 	names = make([]string, 0, numFlagBitsDefined)
 	for f := SafetyFlags(1); f < safetyFlagsLimit; f <<= 1 {
@@ -78,24 +78,26 @@ func (f SafetyFlags) AssertValid() {
 	}
 }
 
-// Convenience function to get the safety of the function which underlies a
+// Safety is a convenience function to get the safety of the function which underlies a
 // builtin.
 func (b *Builtin) Safety() SafetyFlags {
 	return SafetyOfBuiltinFunc(b.fn)
 }
 
-// Get the safety of a function which may be used as the CallInternal method in
-// a Callable.
+// SafetyOfCallableFunc gets the safety of fn, a function which may be used as
+// the CallInternal method in a Callable.
 func SafetyOfCallableFunc(fn func(*Thread, Tuple, []Tuple) (Value, error)) SafetyFlags {
 	return function(reflect.ValueOf(fn).Pointer()).safety()
 }
 
-// Get the safety of a function which may be wrapped into a Builtin.
+// SafetyOfBuiltinFunc gets the safety of fn, a function which may be wrapped
+// into a Builtin.
 func SafetyOfBuiltinFunc(fn func(*Thread, *Builtin, Tuple, []Tuple) (Value, error)) SafetyFlags {
 	return function(reflect.ValueOf(fn).Pointer()).safety()
 }
 
-// Get the safety of an arbitrarily-type function at a given location
+// Safety gets the safety flags of an arbitrarily-typed function at a given
+// location.
 func (fn function) safety() (flags SafetyFlags) {
 	if knownSafety != nil {
 		flags = knownSafety[fn]
@@ -104,25 +106,25 @@ func (fn function) safety() (flags SafetyFlags) {
 }
 
 // Convenience function to declare the safety of the function which underlies a
-// builtin.
+// builtin. Panics if passed flags are not valid.
 func (b *Builtin) DeclareSafety(flags SafetyFlags) {
 	DeclareBuiltinFuncSafety(b.fn, flags)
 }
 
 // Declare the safety of fn, a function which may be used as the CallInternal
-// method in a Callable, as flags.
+// method in a Callable, as flags. Panics if passed flags are not valid.
 func DeclareCallableFuncSafety(fn func(*Thread, Tuple, []Tuple) (Value, error), flags SafetyFlags) {
 	function(reflect.ValueOf(fn).Pointer()).declareSafety(flags)
 }
 
 // Declare the safety of fn, a function which may be wrapped into a Builtin, as
-// flags.
+// flags. Panics if passed flags are not valid.
 func DeclareBuiltinFuncSafety(fn func(*Thread, *Builtin, Tuple, []Tuple) (Value, error), flags SafetyFlags) {
 	function(reflect.ValueOf(fn).Pointer()).declareSafety(flags)
 }
 
 // Declare that the safety of an arbitrarily typed function at a given location
-// is flags.
+// is flags. Panics of passed flags are not valid.
 //
 // The first time this function is called for a given function, the value flags
 // is recorded against it exactly. Otherwise, on the second and later calls for
@@ -133,7 +135,7 @@ func DeclareBuiltinFuncSafety(fn func(*Thread, *Builtin, Tuple, []Tuple) (Value,
 // are defined. As these are represented by a set of upvalues and a pointer to
 // the base function, and as safety is defined against these base functions
 // only, safety declarations on different closures may intereact. The strongest
-// guarantee available for a set of same-base closures, _C_, is the
+// guarantee available for a set of same-base closures, _C,_ is the
 // intersection of all safety flags of the closures in _C,_ which can cause the
 // safety guarantees of closures in _C_ to be weakened, which may lead to their
 // being rejected at runtime. If problematic, this can sometimes be remedied by
