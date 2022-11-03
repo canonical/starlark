9ecd2946e248e3119c08cd6ae24012d7707107b1
Marco Manino <marco.manino@canonical.com>
2022-11-03 17:55:30 +0100

    Proof of concept for testing the test framework

--- startest/startest_test.go
+++ startest/startest_test.go
@@ -1,6 +1,11 @@
 package startest_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/canonical/starlark/starlark"
+	"github.com/canonical/starlark/startest"
+)
 
 func TestPredeclareds(t *testing.T) {
 }
@@ -9,4 +14,30 @@ func TestExpectError(t *testing.T) {
 }
 
 func TestMaxAllocs(t *testing.T) {
+}
+
+func TestPredeclared(t *testing.T) {
+	st := startest.From(t)
+	testBuiltin := starlark.NewBuiltin("testBuiltin", func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		return starlark.None, nil
+	})
+	testValue := starlark.String("value")
+
+	st.AddBuiltin(testBuiltin)
+
+	st.AddValue("testValue", testValue)
+
+	st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
+		if v, ok := sd["testBuiltin"]; !ok {
+			st.Error("testBuiltin not found")
+		} else if v != testBuiltin {
+			st.Error("wrong value expected")
+		}
+
+		if v, ok := sd["testValue"]; !ok {
+			st.Error("testValue not found")
+		} else if v != testValue {
+			st.Error("wrong value expected")
+		}
+	})
 }
