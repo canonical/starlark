43d5d77edae423c7d137a3e69d21efa4c1b89665
Ed Jones <edjones98@gmail.com>
2022-11-03 17:41:06 +0100

    Simplify value caching, repeat tests to fill time and space

--- startest/startest.go
+++ startest/startest.go
@@ -1,9 +1,10 @@
 package startest
 
 import (
-	"math"
 	"runtime"
 	"testing"
+	"time"
+	"unsafe"
 
 	"github.com/canonical/starlark/starlark"
 	"gopkg.in/check.v1"
@@ -19,6 +20,8 @@ type allocTest struct {
 	predefined  starlark.StringDict
 	maxAllocs   uint64
 	expectedErr string
+	tracked     []interface{}
+	N           int
 	testBase
 }
 
@@ -53,23 +56,25 @@ func (test *allocTest) Expect(err string) {
 }
 
 func (test *allocTest) RunBuiltin(fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) {
-	test.RunThread(func(th *starlark.Thread, globals starlark.StringDict) interface{} {
-		result, err := starlark.Call(th, fn, args, kwargs)
-		if err != nil {
-			test.Error(err)
-		}
+	test.RunThread(func(th *starlark.Thread, globals starlark.StringDict) {
+		for i := 0; i < test.N; i++ {
+			result, err := starlark.Call(th, fn, args, kwargs)
+			if err != nil {
+				test.Error(err)
+			}
 
-		return result
+			test.Track(result)
+		}
 	})
 }
 
-func (test *allocTest) RunThread(fn func(*starlark.Thread, starlark.StringDict) interface{}) {
+func (test *allocTest) RunThread(fn func(*starlark.Thread, starlark.StringDict)) {
 	thread := &starlark.Thread{}
-	_, measured := measureMemory(func() interface{} {
-		return fn(thread, test.predefined)
+	measured := test.measureMemory(func() {
+		fn(thread, test.predefined)
 	})
 
-	if test.maxAllocs != 0 {
+	if !test.Failed() && test.maxAllocs != 0 {
 		if measured > test.maxAllocs {
 			test.Errorf("too many measured allocations")
 		}
@@ -79,40 +84,70 @@ func (test *allocTest) RunThread(fn func(*starlark.Thread, starlark.StringDict) 
 	}
 }
 
-func measureMemory(generate func() interface{}) (interface{}, uint64) {
-	measured := uint64(math.MaxUint64)
-	var result interface{}
+func (test *allocTest) Track(v ...interface{}) {
+	test.tracked = append(test.tracked, v...)
+}
 
-	for i := 0; i < 20; i++ {
+func (test *allocTest) measureMemory(fn func()) uint64 {
+	defer func() { test.tracked = make([]interface{}, 0) }()
+
+	startNano := time.Now().Nanosecond()
+
+	const nMax = 100_000
+	const memoryTarget = 100 * 2 << 20
+	const timeMax = 1000
+
+	var memoryUsed int64
+	var valueTrackerOverhead int64
+	test.N = 0
+	for n := int64(1); !test.Failed() && memoryUsed-valueTrackerOverhead < memoryTarget && n < nMax && (time.Now().Nanosecond()-startNano) < timeMax; {
+		valueTrackerOverhead = int64(cap(test.tracked)) * int64(unsafe.Sizeof(interface{}(nil)))
+		last := n
+		prevIters := int64(test.N)
+		prevMemory := memoryUsed
+		if prevMemory <= 0 {
+			prevMemory = 1
+		}
+		n = memoryTarget * prevIters / prevMemory
+		n += n / 5
+		maxGrowth := last * 100
+		minGrowth := last + 1
+		if n > maxGrowth {
+			n = maxGrowth
+		} else if n < minGrowth {
+			n = minGrowth
+		}
+
+		if n > nMax {
+			n = nMax
+		}
+
+		test.N = int(n)
+
 		var before, after runtime.MemStats
 		runtime.GC()
 		runtime.GC()
 		runtime.ReadMemStats(&before)
 
-		iterationResult := generate()
+		fn()
 
 		runtime.GC()
 		runtime.GC()
 		runtime.ReadMemStats(&after)
 
-		iterationMeasure := uint64(after.Alloc - before.Alloc)
-
-		if iterationMeasure == measured {
-			break
-		} else if iterationMeasure < measured {
-			measured = iterationMeasure
-			result = iterationResult
+		iterationMeasure := int64(after.Alloc - before.Alloc)
+		if iterationMeasure <= 0 {
+			memoryUsed++
+		} else {
+			memoryUsed += iterationMeasure
 		}
 	}
 
-	return result, measured
-}
-
-func (test *allocTest) N() int {
-	if b, ok := test.testBase.(*testing.B); ok {
-		return b.N
+	if test.Failed() {
+		return 0
 	}
-	return 1
+
+	return uint64(float64(memoryUsed-valueTrackerOverhead) / float64(test.N))
 }
 
 func (test *allocTest) run(fn func() error) {
