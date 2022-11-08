0c75d1cf159ae42aad493f51165cbab606ba83be
Ed Jones <ed.jones@canonical.com>
2022-11-08 12:20:14 +0000

    Add convenience functions to convert go values to starlark ones

--- startest/startest.go
+++ startest/startest.go
@@ -1,7 +1,9 @@
 package startest
 
 import (
+	"fmt"
 	"math"
+	"reflect"
 	"runtime"
 	"testing"
 	"time"
@@ -157,4 +159,103 @@ func (test *starTest) measureMemory(fn func()) uint64 {
 	memoryUsed -= valueTrackerOverhead
 
 	return uint64(memoryUsed)
+}
+
+func (test *starTest) MakeArgs(raw ...interface{}) (starlark.Tuple, error) {
+	args := make(starlark.Tuple, len(raw))
+
+	for _, rawArg := range raw {
+		if arg, err := ToValue(rawArg); err == nil {
+			args = append(args, arg)
+		} else {
+			return nil, err
+		}
+	}
+	return args, nil
+}
+
+func (test *starTest) MakeKwargs(raw map[string]interface{}) ([]starlark.Tuple, error) {
+	kwargs := make([]starlark.Tuple, len(raw))
+
+	for k, v := range raw {
+		k, err := ToValue(k)
+		if err != nil {
+			return nil, err
+		}
+
+		v, err := ToValue(v)
+		if err != nil {
+			return nil, err
+		}
+
+		kwargs = append(kwargs, starlark.Tuple{k, v})
+	}
+
+	return kwargs, nil
+}
+
+// ToValue converts go values to starlark ones. Handles arrays, slices,
+// interfaces, maps and all scalar types except int32.
+func ToValue(in interface{}) (starlark.Value, error) {
+	// Special behaviours
+	if in, ok := in.(starlark.Value); ok {
+		return in, nil
+	}
+	if c, ok := in.(rune); ok {
+		return starlark.String(c), nil
+	}
+
+	var inVal reflect.Value
+	if v, ok := in.(reflect.Value); ok {
+		inVal = v
+	} else {
+		inVal = reflect.ValueOf(in)
+	}
+
+	kind := inVal.Kind()
+	switch kind {
+	case reflect.Invalid:
+		return starlark.None, nil
+	case reflect.Bool:
+		return starlark.Bool(inVal.Bool()), nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return starlark.MakeInt(int(inVal.Int())), nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return starlark.MakeInt(int(inVal.Uint())), nil
+	case reflect.Float32, reflect.Float64:
+		return starlark.Float(inVal.Float()), nil
+	case reflect.Array, reflect.Slice:
+		len := inVal.Len()
+		elems := make([]starlark.Value, len)
+		for i := 0; i < len; i++ {
+			var err error
+			if elems[i], err = ToValue(inVal.Index(i)); err != nil {
+				return nil, err
+			}
+		}
+		return starlark.NewList(elems), nil
+	case reflect.Map:
+		d := starlark.NewDict(inVal.Len())
+		iter := inVal.MapRange()
+		for iter.Next() {
+			k, err := ToValue(iter.Key())
+			if err != nil {
+				return nil, err
+			}
+			v, err2 := ToValue(iter.Value())
+			if err2 != nil {
+				return nil, err2
+			}
+			d.SetKey(k, v)
+		}
+		return d, nil
+	case reflect.String:
+		return starlark.String(inVal.String()), nil
+	case reflect.Interface:
+		return ToValue(inVal.Interface())
+	case reflect.Ptr:
+		return ToValue(inVal.Elem())
+	default:
+		return nil, fmt.Errorf("Cannot automatically convert a value of kind %v to a starlark.Value: encountered %v", kind, in)
+	}
 }
--- startest/startest_test.go
+++ startest/startest_test.go
@@ -1,8 +1,11 @@
 package startest_test
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
+	starlarktime "github.com/canonical/starlark/lib/time"
 	"github.com/canonical/starlark/starlark"
 	"github.com/canonical/starlark/startest"
 )
@@ -100,4 +103,80 @@ func TestThread(t *testing.T) {
 			st.Error("wrong value expected")
 		}
 	})
+}
+
+func TestMakeArgs(t *testing.T) {
+	// TODO: complete me!
+}
+
+func TestMakeKwargs(t *testing.T) {
+	// TODO: complete me!
+}
+
+func TestConvert(t *testing.T) {
+	type conversionTest struct {
+		from interface{}
+		to   starlark.Value
+	}
+
+	str := "foobar"
+	strPtr := &str
+	value := starlarktime.Duration(time.Nanosecond)
+
+	tests := []conversionTest{
+		{from: value, to: value},
+		{from: nil, to: starlark.None},
+		{from: true, to: starlark.Bool(true)},
+		{from: -1, to: starlark.MakeInt(-1)},
+		{from: 'a', to: starlark.String("a")},
+		{from: str, to: starlark.String(str)},
+		{from: &str, to: starlark.String(str)},
+		{from: &strPtr, to: starlark.String(str)},
+		{from: rune(10), to: starlark.String("\n")},
+		{from: byte(10), to: starlark.MakeInt(10)},
+		{from: int(10), to: starlark.MakeInt(10)},
+		{from: int8(10), to: starlark.MakeInt(10)},
+		{from: int16(10), to: starlark.MakeInt(10)},
+		{from: int64(10), to: starlark.MakeInt(10)},
+		{from: uint(10), to: starlark.MakeInt(10)},
+		{from: uint8(10), to: starlark.MakeInt(10)},
+		{from: uint16(10), to: starlark.MakeInt(10)},
+		{from: uint32(10), to: starlark.MakeInt(10)},
+		{from: uint64(10), to: starlark.MakeInt(10)},
+		{from: uintptr(10), to: starlark.MakeInt(10)},
+		{from: float32(2.5), to: starlark.Float(2.5)},
+		{from: float64(3.14), to: starlark.Float(3.14)},
+		{
+			from: []string{"foo", "bar"},
+			to:   starlark.NewList(append(make([]starlark.Value, 0), starlark.String("foo"), starlark.String("bar"))),
+		}, {
+			from: [...]string{"foo", "bar"},
+			to:   starlark.NewList(append(make([]starlark.Value, 0), starlark.String("foo"), starlark.String("bar"))),
+		}, {
+			from: map[string]string{"foo": "bar"},
+			to: func() starlark.Value {
+				dict := starlark.NewDict(1)
+				dict.SetKey(starlark.String("foo"), starlark.String("bar"))
+				return dict
+			}(),
+		}, {
+			from: map[string][]string{"foo": {"bar", "baz"}},
+			to: func() starlark.Value {
+				dict := starlark.NewDict(1)
+				dict.SetKey(starlark.String("foo"), starlark.NewList(append(make([]starlark.Value, 0, 2), starlark.String("bar"), starlark.String("baz"))))
+				return dict
+			}(),
+		}, {
+			from: []starlark.String{starlark.String("foo"), starlark.String("bar")},
+			to:   starlark.NewList(append(make([]starlark.Value, 0), starlark.String("foo"), starlark.String("bar"))),
+		},
+	}
+
+	for _, test := range tests {
+		if converted, err := startest.ToValue(test.from); err != nil {
+			t.Error(err)
+		} else if !reflect.DeepEqual(converted, test.to) {
+			t.Errorf("Incorrect starlark value conversion: expected %v but got %v", test.to, converted)
+		}
+	}
 }
