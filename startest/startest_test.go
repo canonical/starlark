58237c3c3a3065a95e3327f6202ca77426d5b6d4
Ed Jones <ed.jones@canonical.com>
2022-11-09 18:05:48 +0000

    Test each test base

--- startest/startest_test.go
+++ startest/startest_test.go
@@ -18,101 +18,109 @@ type checkSuite struct{}
 
 var _ = check.Suite(&checkSuite{})
 
-func TestRunBuiltin(t *testing.T) {
-	st := startest.From(t)
-	expectedArgs := starlark.Tuple{starlark.String("a"), starlark.String("b"), starlark.String("c")}
-	k := starlark.String("k")
-	v := starlark.String("v")
+func (*checkSuite) TestRunBuiltin(c *check.C) {
+	for _, base := range []startest.TestBase{&testing.T{}, &testing.B{}, c} {
+		st := startest.From(base)
 
-	var builtin *starlark.Builtin
-	builtin = starlark.NewBuiltin(
-		"test",
-		func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-			if fn != builtin {
-				st.Error("Wrong builtin")
+		expectedArgs := starlark.Tuple{starlark.String("a"), starlark.String("b"), starlark.String("c")}
+		k := starlark.String("k")
+		v := starlark.String("v")
+
+		var builtin *starlark.Builtin
+		builtin = starlark.NewBuiltin(
+			"test",
+			func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+				if fn != builtin {
+					st.Error("Wrong builtin")
+				}
+
+				if thread == nil {
+					st.Error("Thread is not available")
+				}
+
+				if !reflect.DeepEqual(args, expectedArgs) {
+					st.Errorf("Incorrect arguments: expected %v but got %v", expectedArgs, args)
+				}
+
+				if len(kwargs) != 1 || len(kwargs[0]) != 2 || kwargs[0][0] != k || kwargs[0][1] != v {
+					st.Error("Wrong kw arguments received")
+				}
+
+				return starlark.None, nil
+			},
+		)
+
+		st.AddArgs(expectedArgs[0])
+		st.AddArgs(expectedArgs[1:]...)
+		st.AddKwarg(string(k), v)
+		st.RunBuiltin(builtin)
+	}
+}
+
+func (*checkSuite) TestTrack(c *check.C) {
+	for _, base := range []startest.TestBase{&testing.T{}, &testing.B{}, c} {
+		st := startest.From(base)
+
+		// Check for a non-allocating routine
+		st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
+			for i := 0; i < st.N; i++ {
+				st.Track(nil)
+			}
+		})
+
+		// Check for exact measuring
+		st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
+			for i := 0; i < st.N; i++ {
+				st.Track(new(int32))
+				t.AddAllocs(4)
+			}
+		})
+
+		// Check for over eximations
+		st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
+			for i := 0; i < st.N; i++ {
+				st.Track(new(int32))
+				t.AddAllocs(20)
+			}
+		})
+	}
+}
+
+func (*checkSuite) TestThread(c *check.C) {
+	for _, base := range []startest.TestBase{&testing.T{}, &testing.B{}, c} {
+		st := startest.From(base)
+
+		testBuiltin := starlark.NewBuiltin("testBuiltin", func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+			return starlark.None, nil
+		})
+		testValue := starlark.String("value")
+
+		st.AddBuiltin(testBuiltin)
+
+		st.AddValue("testValue", testValue)
+
+		st.RunThread(func(thread *starlark.Thread, sd starlark.StringDict) {
+			if sd == nil {
+				st.Error("Received a nil environment")
 			}
 
 			if thread == nil {
-				st.Error("Thread is not available")
+				st.Error("Received a nil thread")
 			}
 
-			if !reflect.DeepEqual(args, expectedArgs) {
-				st.Errorf("Incorrect arguments: expected %v but got %v", expectedArgs, args)
+			if v, ok := sd["testBuiltin"]; !ok {
+				st.Error("testBuiltin not found")
+			} else if v != testBuiltin {
+				st.Error("wrong value expected")
 			}
 
-			if len(kwargs) != 1 || len(kwargs[0]) != 2 || kwargs[0][0] != k || kwargs[0][1] != v {
-				st.Error("Wrong kw arguments received")
+			if v, ok := sd["testValue"]; !ok {
+				st.Error("testValue not found")
+			} else if v != testValue {
+				st.Error("wrong value expected")
 			}
-
-			return starlark.None, nil
-		},
-	)
-
-	st.AddArgs(expectedArgs[0])
-	st.AddArgs(expectedArgs[1:]...)
-	st.AddKwarg(string(k), v)
-	st.RunBuiltin(builtin)
-}
-
-func TestTrack(t *testing.T) {
-	st := startest.From(t)
-
-	// Check for a non-allocating routine
-	st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
-		for i := 0; i < st.N; i++ {
-			st.Track(nil)
-		}
-	})
-
-	// Check for exact measuring
-	st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
-		for i := 0; i < st.N; i++ {
-			st.Track(new(int32))
-			t.AddAllocs(4)
-		}
-	})
-
-	// Check for over eximations
-	st.RunThread(func(t *starlark.Thread, sd starlark.StringDict) {
-		for i := 0; i < st.N; i++ {
-			st.Track(new(int32))
-			t.AddAllocs(20)
-		}
-	})
-}
-
-func TestThread(t *testing.T) {
-	st := startest.From(t)
-	testBuiltin := starlark.NewBuiltin("testBuiltin", func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-		return starlark.None, nil
-	})
-	testValue := starlark.String("value")
-
-	st.AddBuiltin(testBuiltin)
-
-	st.AddValue("testValue", testValue)
-
-	st.RunThread(func(thread *starlark.Thread, sd starlark.StringDict) {
-		if sd == nil {
-			st.Error("Received a nil environment")
-		}
-
-		if thread == nil {
-			st.Error("Received a nil thread")
-		}
-
-		if v, ok := sd["testBuiltin"]; !ok {
-			st.Error("testBuiltin not found")
-		} else if v != testBuiltin {
-			st.Error("wrong value expected")
-		}
-
-		if v, ok := sd["testValue"]; !ok {
-			st.Error("testValue not found")
-		} else if v != testValue {
-			st.Error("wrong value expected")
-		}
-	})
+		})
+	}
 }
 
 func (*checkSuite) TestFailed(c *check.C) {
