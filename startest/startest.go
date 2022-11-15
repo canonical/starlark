704c4cf76bc8d888d550922c469a4f39afcc1bad
kcza <ed.jones@canonical.com>
2022-11-15 11:34:42 +0000

    Memory bounding test framework (#8)

---
+++ startest/startest.go
@@ -0,0 +1,143 @@
+package startest
+
+import (
+	"math"
+	"runtime"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/canonical/starlark/starlark"
+	"gopkg.in/check.v1"
+)
+
+type TestBase interface {
+	Error(err ...interface{})
+	Errorf(format string, err ...interface{})
+	Failed() bool
+	Log(args ...interface{})
+	Logf(fmt string, args ...interface{})
+}
+
+type S struct {
+	maxAllocs uint64
+	alive     []interface{}
+	N         int
+	TestBase
+}
+
+var _ TestBase = &testing.T{}
+var _ TestBase = &testing.B{}
+var _ TestBase = &check.C{}
+
+// From returns a new starTest instance with a given test base.
+func From(base TestBase) *S {
+	return &S{TestBase: base, maxAllocs: math.MaxUint64}
+}
+
+// SetMaxAllocs optionally sets the max allocations allowed per test.N
+func (test *S) SetMaxAllocs(maxAllocs uint64) {
+	test.maxAllocs = maxAllocs
+}
+
+// RunThread tests a function which has access to a starlark thread and a global environment
+func (test *S) RunThread(fn func(*starlark.Thread)) {
+	thread := &starlark.Thread{}
+
+	memorySum, nSum := test.measureMemory(func() {
+		fn(thread)
+	})
+
+	if test.Failed() {
+		return
+	}
+
+	meanMeasured := memorySum / nSum
+
+	if meanMeasured > test.maxAllocs {
+		test.Errorf("measured memory is above maximum (%d > %d)", meanMeasured, test.maxAllocs)
+	}
+
+	if meanDeclared := thread.Allocs() / nSum; meanDeclared > test.maxAllocs {
+		test.Errorf("declared allocations are above maximum (%d > %d)", meanDeclared, test.maxAllocs)
+	}
+
+	if test.maxAllocs != math.MaxUint64 {
+		if meanMeasured > thread.Allocs() {
+			test.Errorf("measured memory is above declared allocations (%d > %d)", meanMeasured, thread.Allocs())
+		}
+	}
+}
+
+// KeepAlive causes the memory of the passed objects to be measured
+func (test *S) KeepAlive(values ...interface{}) {
+	test.alive = append(test.alive, values...)
+}
+
+func (test *S) measureMemory(fn func()) (memorySum, nSum uint64) {
+	startNano := time.Now().Nanosecond()
+
+	const nMax = 100_000
+	const memoryMax = 100 * 2 << 20
+	const timeMax = 1e9
+
+	var memoryUsed uint64
+	var valueTrackerOverhead uint64
+	test.N = 0
+	nSum = 0
+
+	for n := uint64(0); !test.Failed() && memoryUsed-valueTrackerOverhead < memoryMax && n < nMax && (time.Now().Nanosecond()-startNano) < timeMax; {
+		last := n
+		prevIters := uint64(test.N)
+		prevMemory := memoryUsed
+		if prevMemory <= 0 {
+			prevMemory = 1
+		}
+		n = memoryMax * prevIters / prevMemory
+		n += n / 5
+		maxGrowth := last * 100
+		minGrowth := last + 1
+		if n > maxGrowth {
+			n = maxGrowth
+		} else if n < minGrowth {
+			n = minGrowth
+		}
+
+		if n > nMax {
+			n = nMax
+		}
+
+		test.N = int(n)
+		nSum += n
+
+		var before, after runtime.MemStats
+		runtime.GC()
+		runtime.GC()
+		runtime.ReadMemStats(&before)
+
+		fn()
+
+		runtime.GC()
+		runtime.GC()
+		runtime.ReadMemStats(&after)
+
+		iterationMeasure := int64(after.Alloc - before.Alloc)
+		valueTrackerOverhead += uint64(cap(test.alive)) * uint64(unsafe.Sizeof(interface{}(nil)))
+		test.alive = nil
+		if iterationMeasure > 0 {
+			memoryUsed += uint64(iterationMeasure)
+		}
+	}
+
+	if test.Failed() {
+		return 0, 1
+	}
+
+	if valueTrackerOverhead > memoryUsed {
+		memoryUsed = 0
+	} else {
+		memoryUsed -= valueTrackerOverhead
+	}
+
+	return memoryUsed, nSum
+}
---
+++ startest/startest_test.go
@@ -0,0 +1,99 @@
+package startest_test
+
+import (
+	"testing"
+
+	"github.com/canonical/starlark/starlark"
+	"github.com/canonical/starlark/startest"
+)
+
+func TestKeepAlive(t *testing.T) {
+	// Check for a non-allocating routine
+	t.Run("check=non-allocating", func(t *testing.T) {
+		st := startest.From(t)
+		st.SetMaxAllocs(0)
+		st.RunThread(func(thread *starlark.Thread) {
+			for i := 0; i < st.N; i++ {
+				st.KeepAlive(nil)
+			}
+		})
+	})
+
+	// Check for exact measuring
+	t.Run("check=exact", func(t *testing.T) {
+		st := startest.From(t)
+		st.SetMaxAllocs(4)
+		st.RunThread(func(thread *starlark.Thread) {
+			for i := 0; i < st.N; i++ {
+				st.KeepAlive(new(int32))
+				thread.AddAllocs(4)
+			}
+		})
+	})
+
+	// Check for over estimations
+	t.Run("check=over-estimation", func(t *testing.T) {
+		dummyT := testing.T{}
+		st := startest.From(&dummyT)
+		st.SetMaxAllocs(4)
+		st.RunThread(func(thread *starlark.Thread) {
+			for i := 0; i < st.N; i++ {
+				st.KeepAlive(new(int32))
+				thread.AddAllocs(20)
+			}
+		})
+		if !dummyT.Failed() {
+			t.Error("Expected allocation test failure")
+		}
+	})
+
+	// Check for too many allocs
+	t.Run("check=over-allocation", func(t *testing.T) {
+		dummyT := testing.T{}
+		st := startest.From(&dummyT)
+		st.SetMaxAllocs(4)
+		st.RunThread(func(thread *starlark.Thread) {
+			for i := 0; i < st.N; i++ {
+				st.KeepAlive(make([]int32, 10))
+				if err := thread.AddAllocs(4); err != nil {
+					t.Errorf("Unexpected error: %v", err)
+					return
+				}
+			}
+		})
+		if !dummyT.Failed() {
+			t.Error("Expected allocation test failure")
+		}
+	})
+}
+
+func TestThread(t *testing.T) {
+	st := startest.From(t)
+	st.RunThread(func(thread *starlark.Thread) {
+		if thread == nil {
+			st.Error("Received a nil thread")
+		}
+	})
+}
+
+func TestFailed(t *testing.T) {
+	dummyT := &testing.T{}
+
+	st := startest.From(dummyT)
+
+	if st.Failed() {
+		t.Error("Startest reported that it failed prematurely")
+	}
+
+	st.Log("foobar")
+
+	if st.Failed() {
+		t.Error("Startest reported that it failed prematurely")
+	}
+
+	st.Error("snafu")
+
+	if !st.Failed() {
+		t.Error("Startest did not report that it had failed")
+	}
+}
