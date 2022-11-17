7b7c479e8f69d69e9fa45a4ee8c33061f7ddd0e0
Ed Jones <ed.jones@canonical.com>
2022-11-17 18:08:51 +0000

    Memory tracking `string.isalnum` builtin

--- starlark/allocation_test.go
+++ starlark/allocation_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/canonical/starlark/starlark"
+	"github.com/canonical/starlark/startest"
 )
 
 func TestDefaultAllocMaxIsUnbounded(t *testing.T) {
@@ -284,4 +285,24 @@ func TestAddAllocsCancelledRejection(t *testing.T) {
 	} else if allocs := thread.Allocs(); allocs != 0 {
 		t.Errorf("Changes were recorded against cancelled thread: expected 0 allocations, got %v", allocs)
 	}
+}
+
+func TestStringIsalnumAllocations(t *testing.T) {
+	string_isalnum, _ := starlark.String("hello, world!").Attr("isalnum")
+	if string_isalnum == nil {
+		t.Errorf("No such method: string.isalnum")
+		return
+	}
+
+	s := startest.From(t)
+	s.SetMaxAllocs(0)
+	s.RunThread(func(thread *starlark.Thread) {
+		for i := 0; i < s.N; i++ {
+			result, err := starlark.Call(thread, string_isalnum, nil, nil)
+			if err != nil {
+				s.Error(err)
+			}
+			s.KeepAlive(result)
+		}
+	})
 }
