c103a7e14cf35de2879e94febfcd6e974699e15d
Ed Jones <ed.jones@canonical.com>
2023-01-27 15:56:10 +0000

    Explicitly require `MemSafe`

--- starlark/library_test.go
+++ starlark/library_test.go
@@ -202,6 +202,7 @@ func TestChrAllocs(t *testing.T) {
 
 	st := startest.From(t)
 	st.RequireSafety(starlark.MemSafe)
+	st.RequireSafety(starlark.MemSafe)
 	st.SetMaxAllocs(32)
 	st.RunThread(func(thread *starlark.Thread) {
 		for i := 0; i < st.N; i++ {
