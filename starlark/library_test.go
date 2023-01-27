8bb7b0c65196fd4b26b1f58668ba34b4ba210b6f
Ed Jones <ed.jones@canonical.com>
2023-01-27 15:37:07 +0000

    Explicitly require `MemSafe`

--- starlark/library_test.go
+++ starlark/library_test.go
@@ -252,6 +252,7 @@ func TestStringIslowerAllocs(t *testing.T) {
 	}
 
 	st := startest.From(t)
+	st.RequireSafety(starlark.MemSafe)
 	st.SetMaxAllocs(0)
 	st.RunThread(func(thread *starlark.Thread) {
 		for i := 0; i < st.N; i++ {
