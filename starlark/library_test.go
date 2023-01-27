5b741684aaeb90ec2f5563ab59a003503e46fcde
Ed Jones <ed.jones@canonical.com>
2023-01-27 15:38:38 +0000

    Explicitly require `MemSafe`

--- starlark/library_test.go
+++ starlark/library_test.go
@@ -249,6 +249,7 @@ func TestStringIsdigitAllocs(t *testing.T) {
 	}
 
 	st := startest.From(t)
+	st.RequireSafety(starlark.MemSafe)
 	st.SetMaxAllocs(0)
 	st.RunThread(func(thread *starlark.Thread) {
 		for i := 0; i < st.N; i++ {
