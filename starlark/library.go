f64dc22bba4bd14866ce163b338c49f89a657b5d
Ed Jones <ed.jones@canonical.com>
2023-05-10 12:26:14 +0100

    Pass pointer to `NewSafeAppender`

--- starlark/library.go
+++ starlark/library.go
@@ -502,7 +502,7 @@ func enumerate(thread *Thread, _ *Builtin, args Tuple, kwargs []Tuple) (Value, e
 	} else {
 		// non-sequence (unknown length)
 		pairCost := EstimateSize(Tuple{MakeInt(0), nil})
-		appender := NewSafeAppender(thread, pairs)
+		appender := NewSafeAppender(thread, &pairs)
 		for i := 0; iter.Next(&x); i++ {
 			if err := thread.AddAllocs(pairCost); err != nil {
 				return nil, err
