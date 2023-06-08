cf0948e08820a26eef61703359013685d211dea5
Marco Manino <mrcmnn@gmail.com>
2023-06-08 09:11:29 +0200

    Split testing now that it's possible

--- starlark/interp_test.go
+++ starlark/interp_test.go
@@ -11,8 +11,11 @@ func TestTupleCreation(t *testing.T) {
 	st := startest.From(t)
 	st.RequireSafety(starlark.MemSafe)
 	st.RunString(`
-		for _ in range(st.n):
-			st.keep_alive(()) # empty
+		for _ in st.ntimes():
+			st.keep_alive(())
+	`)
+	st.RunString(`
+		for _ in st.ntimes():
 			st.keep_alive((1, "2", 3.0))
 	`)
 }
