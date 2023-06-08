64ce61cb543cab0dc2a61a477a035dad136c4878
Marco Manino <mrcmnn@gmail.com>
2023-06-08 09:22:29 +0200

    using ntimes

--- starlark/interp_test.go
+++ starlark/interp_test.go
@@ -11,8 +11,11 @@ func TestListCreation(t *testing.T) {
 	st := startest.From(t)
 	st.RequireSafety(starlark.MemSafe)
 	st.RunString(`
-		for _ in range(st.n):
-			st.keep_alive([]) # empty
+		for _ in st.ntimes():
+			st.keep_alive([])
+	`)
+	st.RunString(`
+		for _ in st.ntimes():
 			st.keep_alive([ False, 1, "2", 3.0 ])
 	`)
 }
