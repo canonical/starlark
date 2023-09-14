e98cf5b57b7882cc1275ae33b72c918ee7002a8b
Marco Manino <mrcmnn@gmail.com>
2023-09-14 09:44:11 +0200

    Make dict.items builtin CPUSafe

--- starlark/library.go
+++ starlark/library.go
@@ -138,7 +138,7 @@ var (
 	dictMethodSafeties = map[string]Safety{
 		"clear":      MemSafe | IOSafe | CPUSafe,
 		"get":        MemSafe | IOSafe | CPUSafe,
-		"items":      MemSafe | IOSafe,
+		"items":      MemSafe | IOSafe | CPUSafe,
 		"keys":       MemSafe | IOSafe,
 		"pop":        MemSafe | IOSafe | CPUSafe,
 		"popitem":    MemSafe | IOSafe | CPUSafe,
@@ -1745,6 +1745,9 @@ func dict_items(thread *Thread, b *Builtin, args Tuple, kwargs []Tuple) (Value, 
 	itemSize := EstimateMakeSize([]Value{Tuple{}}, len)
 	resultSize := EstimateSize(&List{})
 	if err := thread.AddAllocs(itemSize + arraySize + resultSize); err != nil {
+		return nil, err
+	}
+	if err := thread.AddExecutionSteps(int64(len)); err != nil {
 		return nil, err
 	}
 	tupleItemsSize := EstimateMakeSize([]Tuple{}, len)
