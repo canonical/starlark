3c88f6b6a89d0eb3435c8a34f62ea48e02975873
Ed Jones <edjones98@gmail.com>
2023-09-26 10:32:31 +0100

    Fix test table type

--- starlark/eval_test.go
+++ starlark/eval_test.go
@@ -1427,7 +1427,7 @@ func TestSafeBinaryAllocs(t *testing.T) {
 	t.Run("-", func(t *testing.T) {})
 
 	t.Run("*", func(t *testing.T) {
-		tests := []safeBinaryTest{{
+		tests := []safeBinaryAllocTest{{
 			name: "int * int",
 			inputs: func(n int) (starlark.Value, syntax.Token, starlark.Value) {
 				num := starlark.MakeInt(int(math.Sqrt(float64(n))))
