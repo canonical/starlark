87f10885d16487a1983b38279c7cfd6ed3097549
Ed Jones <ed.jones@canonical.com>
2023-10-16 17:09:08 +0100

    Revert name change

--- starlark/eval_test.go
+++ starlark/eval_test.go
@@ -1492,22 +1492,22 @@ func TestSafeBinaryAllocs(t *testing.T) {
 			inputs: func(n int) (starlark.Value, syntax.Token, starlark.Value) {
 				sqrtN := int(math.Sqrt(float64(n)))
 				l := starlark.MakeInt(sqrtN)
-				rightElems := make([]starlark.Value, sqrtN)
-				for i := 0; i < len(rightElems); i++ {
-					rightElems[i] = starlark.String("a")
+				rElems := make([]starlark.Value, sqrtN)
+				for i := 0; i < len(rElems); i++ {
+					rElems[i] = starlark.String("a")
 				}
-				r := starlark.NewList(rightElems)
+				r := starlark.NewList(rElems)
 				return l, syntax.STAR, r
 			},
 		}, {
 			name: "list * int",
 			inputs: func(n int) (starlark.Value, syntax.Token, starlark.Value) {
 				sqrtN := int(math.Sqrt(float64(n)))
-				leftElems := make([]starlark.Value, sqrtN)
-				for i := 0; i < len(leftElems); i++ {
-					leftElems[i] = starlark.String("a")
+				lElems := make([]starlark.Value, sqrtN)
+				for i := 0; i < len(lElems); i++ {
+					lElems[i] = starlark.String("a")
 				}
-				l := starlark.NewList(leftElems)
+				l := starlark.NewList(lElems)
 				r := starlark.MakeInt(sqrtN)
 				return l, syntax.STAR, r
 			},
