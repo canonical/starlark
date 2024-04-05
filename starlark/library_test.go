05d4abbb9eebd3d6b99160a6a624e6127f4f974f
Marco Manino <mrcmnn@gmail.com>
2024-04-05 11:28:23 +0200

    Make repr builtin TimeSafe (#392)

--- starlark/library_test.go
+++ starlark/library_test.go
@@ -3643,6 +3643,10 @@ func TestReprAllocs(t *testing.T) {
 	})
 }
 
+func TestReprCancellation(t *testing.T) {
+	testWriteValueCancellation(t, "repr")
+}
+
 func TestReversedSteps(t *testing.T) {
 	reversed, ok := starlark.Universe["reversed"]
 	if !ok {
