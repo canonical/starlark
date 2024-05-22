da357f5bbd50cee3510b0a5e8e92baca9c54dac0
Marco Manino <mrcmnn@gmail.com>
2024-05-22 08:43:43 +0200

    Adding a comment on the new roundupsize

--- starlark/roundupsize_go122.go
+++ starlark/roundupsize_go122.go
@@ -9,5 +9,8 @@ import _ "unsafe"
 func runtime_roundupsize(size uintptr, noscan bool) (reqSize uintptr)
 
 func roundupsize(size uintptr) uintptr {
+	// In go1.22+, "scan" values (values that contain pointers) require
+	// an additional pointer to the type (8 bytes). It is ok to overestimate
+	// and always add this overhead to avoid checking if a type is noscan.
 	return runtime_roundupsize(size, false)
 }
