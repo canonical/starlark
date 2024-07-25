ed17f77a33f1c54a1d3b54f36cccdd5ae014a1c6
Ed Jones <ed.jones@canonical.com>
2024-07-25 15:28:53 +0100

    Simplify arithmetic implementation

--- starlark/safearithmetic.go
+++ starlark/safearithmetic.go
@@ -6,137 +6,93 @@ import (
 )
 
 func SafeAdd(a, b int) int {
-	switch a {
-	case math.MinInt, math.MaxInt:
+	if a == math.MinInt || a == math.MaxInt {
 		return a
 	}
-	switch b {
-	case math.MinInt, math.MaxInt:
+	if b == math.MinInt || b == math.MaxInt {
 		return b
 	}
 
-	if a < 0 {
-		negatedRet := SafeAdd(-a, -b)
-		switch negatedRet {
-		case math.MaxInt:
-			return math.MinInt
-		case math.MinInt:
-			return math.MaxInt
-		}
-		return -negatedRet
+	if ret := int(uint(a) + uint(b)); !sameSign(a, b) || sameSign(ret, a) {
+		// no overflow possible
+		return ret
 	}
 
-	if b >= 0 {
-		sum, carry := bits.Add(uint(a), uint(b), 0)
-		if sum > math.MaxInt || carry != 0 {
-			return math.MaxInt
-		}
-		return int(sum)
-	}
-
-	if uint(a) < uint(-b) {
-		return math.MinInt
-	}
-	diff, _ := bits.Sub(uint(a), uint(-b), 0)
-	return int(diff)
-}
-
-func SafeAdd64(a, b int64) int64 {
-	switch a {
-	case math.MinInt64, math.MaxInt64:
-		return a
-	}
-	switch b {
-	case math.MinInt64, math.MaxInt64:
-		return b
-	}
-
-	if a < 0 {
-		negatedRet := SafeAdd64(-a, -b)
-		switch negatedRet {
-		case math.MaxInt64:
-			return math.MinInt64
-		case math.MinInt64:
-			return math.MaxInt64
-		}
-		return -negatedRet
-	}
-
-	if b >= 0 {
-		sum, carry := bits.Add64(uint64(a), uint64(b), 0)
-		if sum > math.MaxInt64 || carry != 0 {
-			return math.MaxInt64
-		}
-		return int64(sum)
-	}
-
-	if uint64(a) < uint64(b) {
-		return math.MinInt64
-	}
-	diff, _ := bits.Sub64(uint64(a), uint64(-b), 0)
-	return int64(diff)
-}
-
-func SafeMul(a, b int) int {
-	var retSign int
-	if (a > 0) == (b > 0) {
-		retSign = 1
-	} else {
-		retSign = -1
-	}
-
-	if a == math.MinInt || a == math.MaxInt || b == math.MinInt || b == math.MaxInt {
-		if retSign > 0 {
-			return math.MaxInt
-		} else {
-			return math.MinInt
-		}
-	}
-
-	if a < 0 {
-		a = -a
-	}
-	if b < 0 {
-		b = -b
-	}
-	hi, lo := bits.Mul(uint(a), uint(b))
-	if hi == 0 && lo < math.MaxInt {
-		return retSign * int(lo)
-	}
-	if retSign > 0 {
+	if a >= 0 {
 		return math.MaxInt
 	}
 	return math.MinInt
 }
 
-func SafeMul64(a, b int64) int64 {
-	var retSign int64
-	if (a > 0) == (b > 0) {
-		retSign = 1
-	} else {
-		retSign = -1
+//go:inline
+func sameSign(a, b int) bool {
+	return a^b >= 0
+}
+
+func SafeAdd64(a, b int64) int64 {
+	if a == math.MinInt64 || a == math.MaxInt64 {
+		return a
+	}
+	if b == math.MinInt64 || b == math.MaxInt64 {
+		return b
 	}
 
-	if a == math.MinInt64 || a == math.MaxInt64 || b == math.MinInt64 || b == math.MaxInt64 {
-		if retSign > 0 {
-			return math.MaxInt64
-		} else {
-			return math.MinInt64
-		}
+	if ret := int64(uint64(a) + uint64(b)); !sameSign64(a, b) || sameSign64(ret, a) {
+		// no overflow possible
+		return ret
 	}
 
-	if a < 0 {
-		a = -a
-	}
-	if b < 0 {
-		b = -b
-	}
-	hi, lo := bits.Mul64(uint64(a), uint64(b))
-	if hi == 0 && lo < math.MaxInt64 {
-		return retSign * int64(lo)
-	}
-	if retSign > 0 {
+	if a >= 0 {
 		return math.MaxInt64
 	}
 	return math.MinInt64
+}
+
+//go:inline
+func sameSign64(a, b int64) bool {
+	return a^b >= 0
+}
+
+func SafeMul(a, b int) int {
+	if a == math.MinInt || a == math.MaxInt {
+		return a
+	}
+	if b == math.MinInt || b == math.MaxInt {
+		return b
+	}
+
+	if a == 0 {
+		return 0
+	}
+	if ret := a * b; ret/a == b {
+		// No overflow occurred.
+		return ret
+	}
+
+	if (a >= 0) != (b >= 0) {
+		return math.MinInt
+	}
+	return math.MaxInt
+}
+
+func SafeMul64(a, b int64) int64 {
+	if a == math.MinInt64 || a == math.MaxInt64 {
+		return a
+	}
+	if b == math.MinInt64 || b == math.MaxInt64 {
+		return b
+	}
+
+	if a == 0 {
+		return 0
+	}
+	if ret := a * b; ret/a == b {
+		// No overflow occurred.
+		return ret
+	}
+
+	if (a >= 0) != (b >= 0) {
+		return math.MinInt64
+	}
+	return math.MaxInt64
 }
