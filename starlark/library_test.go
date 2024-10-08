7e17b4991ef722f00fdc298a23ca9ca839996ce8
Marco Manino <mrcmnn@gmail.com>
2024-10-08 18:12:43 +0200

    Relax tests

--- starlark/library_test.go
+++ starlark/library_test.go
@@ -1168,7 +1168,6 @@ func TestChrAllocs(t *testing.T) {
 
 	st := startest.From(t)
 	st.RequireSafety(starlark.MemSafe)
-	st.SetMaxAllocs(32)
 	st.RunThread(func(thread *starlark.Thread) {
 		for i := 0; i < st.N; i++ {
 			args := starlark.Tuple{starlark.MakeInt(97)}
