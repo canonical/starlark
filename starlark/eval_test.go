035c64b7adbda2dab05c0b3105a781dedc862cc5
Marco Manino <mrcmnn@gmail.com>
2025-01-17 10:32:30 +0100

    Pass `SafeInteger`s to `AddAllocs` in eval_test

--- starlark/eval_test.go
+++ starlark/eval_test.go
@@ -1929,7 +1929,7 @@ func TestSafeBinary(t *testing.T) {
 	makeString := func(thread *starlark.Thread, n int) (starlark.Value, error) {
 		if thread != nil {
 			resultSize := starlark.SafeAdd(
-				starlark.EstimateMakeSize([]byte{}, n),
+				starlark.EstimateMakeSize([]byte{}, starlark.SafeInt(n)),
 				starlark.StringTypeOverhead,
 			)
 			if err := thread.AddAllocs(resultSize); err != nil {
@@ -1975,7 +1975,7 @@ func TestSafeBinary(t *testing.T) {
 	makeTuple := func(thread *starlark.Thread, n int) (starlark.Value, error) {
 		if thread != nil {
 			resultSize := starlark.SafeAdd(
-				starlark.EstimateMakeSize([]starlark.Value{}, n),
+				starlark.EstimateMakeSize([]starlark.Value{}, starlark.SafeInt(n)),
 				starlark.EstimateSize(&starlark.List{}),
 			)
 			if err := thread.AddAllocs(resultSize); err != nil {
@@ -2367,7 +2367,7 @@ func TestSafeBinary(t *testing.T) {
 				const format = "%(k)r"
 				if thread != nil {
 					resultSize := starlark.SafeAdd(
-						starlark.EstimateMakeSize([]byte{}, len(format)*n),
+						starlark.EstimateMakeSize([]byte{}, starlark.SafeMul(len(format), n)),
 						starlark.StringTypeOverhead,
 					)
 					if err := thread.AddAllocs(resultSize); err != nil {
@@ -2393,7 +2393,7 @@ func TestSafeBinary(t *testing.T) {
 					return nil, err
 				}
 				if thread != nil {
-					if err := thread.AddAllocs(starlark.EstimateMakeSize(starlark.Tuple{}, 2)); err != nil {
+					if err := thread.AddAllocs(starlark.EstimateMakeSize(starlark.Tuple{}, starlark.SafeInt(2))); err != nil {
 						return nil, err
 					}
 				}
